4e36eb3fe0cf5ee8e644f7646cf64e657d79c4f8
rongxuanHong <389817374@qq.com>
2020-12-12 11:44:12 +0800

    Create install_kernel.sh

---
+++ install_kernel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make modules -j$(getconf _NPROCESSORS_ONLN)
+make -j$(getconf _NPROCESSORS_ONLN)
+make modules_install -j$(getconf _NPROCESSORS_ONLN)
+make install -j$(getconf _NPROCESSORS_ONLN)
