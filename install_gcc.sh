166a0f076c9652314c260a918a2f525fe66b0939
rongxuanHong <389817374@qq.com>
2020-12-16 11:47:40 +0800

    Update install_gcc.sh

--- install_gcc.sh
+++ install_gcc.sh
@@ -39,11 +39,12 @@ cd "gcc-build"
 INTSTALL_PATH=$HOME/gcc/build-gcc-${GCC_VERSION}
 mkdir -p $INSTALL_PATH
 
-$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls -disable-libstdcxx-pch --enable-shared  --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
+#$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls -disable-libstdcxx-pch --enable-shared  --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
+$GCC_PATH/configure --prefix=${INTSTALL_PATH} --with-plugin-ld=/usr/local/bin/ld-plugin --disable-nls -disable-libstdcxx-pch --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
 
 make -j$(getconf _NPROCESSORS_ONLN)
 
-make install -j$(getconf _NPROCESSORS_ONLN)
+make install-no-fixedincludes
 
-echo "export PATH=${INTSTALL_PATH}/bin:\$PATH" >> ~/.bashrc
-source ~/.bashrc
+#echo "export PATH=${INTSTALL_PATH}/bin:\$PATH" >> ~/.bashrc
+#source ~/.bashrc
