d9ddf635d58772a77d65f0cda049e55a5d1e9b37
rongxuanHong <389817374@qq.com>
2020-12-16 20:13:33 +0800

    Update install_gcc.sh

--- install_gcc.sh
+++ install_gcc.sh
@@ -5,7 +5,7 @@ apt-get install -y gcc make libncurses5-dev openssl libssl-dev build-essential p
 HOME=/home/hrx
 cd $HOME
 
-GCC_VERSION=gcc-4.8.5
+GCC_VERSION=gcc-7.4.0
 GMP=gmp-4.3.2
 MPC=mpc-1.0.1
 MPFR=mpfr-2.4.2
@@ -39,7 +39,7 @@ cd "gcc-build"
 INTSTALL_PATH=$HOME/gcc/build-gcc-${GCC_VERSION}
 mkdir -p $INSTALL_PATH
 
-$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --enable-shared  --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
+$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls --disable-libstdcxx-pch --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
 #$GCC_PATH/configure --prefix=${INTSTALL_PATH} --with-plugin-ld=/usr/local/bin/ld-plugin  --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
 
 make -j$(getconf _NPROCESSORS_ONLN)
