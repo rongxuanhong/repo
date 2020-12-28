06c428ab53626c926c2e7da396306fb91820ceed
rongxuanHong <389817374@qq.com>
2020-12-28 19:22:54 +0800

    Update install_gcc.sh

--- install_gcc.sh
+++ install_gcc.sh
@@ -3,48 +3,51 @@
 
 apt-get install -y gcc make libncurses5-dev openssl libssl-dev build-essential pkg-config libc6-dev bison flex libelf-dev
 HOME=/home/hrx
+#SOURCE_BASE=https://mirrors.huaweicloud.com/gnu
+SOURCE_BASE=https://mirrors.tuna.tsinghua.edu.cn/gnu
 cd $HOME
 
-GCC_VERSION=gcc-7.4.0
-GMP=gmp-4.3.2
-MPC=mpc-1.0.1
+GCC_VERSION=gcc-10.2.0
+GMP=gmp-6.1.0
+#GMP=gmp-4.3.2
+MPC=mpc-1.1.0
 #MPFR=mpfr-2.4.2
-MPFR=mpfr-3.1.4
-#if [ ! -f $GCC_VERSION.tar.gz ];then
-    #wget https://mirrors.tuna.tsinghua.edu.cn/gnu/gcc/$GCC_VERSION/$GCC_VERSION.tar.gz && tar -zxvf $GCC_VERSION.tar.gz
-#fi
+MPFR=mpfr-4.0.2
+if [ ! -d $GCC_VERSION ];then
+    wget $SOURCE_BASE/gcc/$GCC_VERSION/$GCC_VERSION.tar.gz && tar -zxvf $GCC_VERSION.tar.gz && rm -f $GCC_VERSION.tar.gz*
+fi
 
 GCC_PATH=$HOME/$GCC_VERSION
 cd $GCC_PATH
 
 
 if [ ! -d "mpfr" ];then
-	wget https://mirrors.tuna.tsinghua.edu.cn/gnu/mpfr/$MPFR.tar.gz
-	tar -zxvf $MPFR.tar.gz && mv $MPFR mpfr && rm -f $MPFR.tar.gz
+        wget $SOURCE_BASE/mpfr/$MPFR.tar.gz
+        tar -zxvf $MPFR.tar.gz && mv $MPFR mpfr && rm -f $MPFR.tar.gz
 fi
 
 if [ ! -d "mpc" ];then
-	wget https://mirrors.tuna.tsinghua.edu.cn/gnu/mpc/$MPC.tar.gz
-	tar -zxvf $MPC.tar.gz && mv $MPC mpc && rm -f $MPC.tar.gz
+        wget $SOURCE_BASE/mpc/$MPC.tar.gz
+        tar -zxvf $MPC.tar.gz && mv $MPC mpc && rm -f $MPC.tar.gz
 fi
 
 if [ ! -d "gmp" ];then
-	wget https://mirrors.tuna.tsinghua.edu.cn/gnu/gmp/$GMP.tar.bz2
-	tar -jxvf $GMP.tar.bz2 && mv $GMP gmp && rm -f $GMP.tar.bz2
+        wget $SOURCE_BASE/gmp/$GMP.tar.bz2
+        tar -jxvf $GMP.tar.bz2 && mv $GMP gmp && rm -f $GMP.tar.bz2
 fi
 
-mkdir -p gcc-build 
+mkdir -p gcc-build
 cd "gcc-build"
 
-INTSTALL_PATH=$HOME/gcc/build-gcc-${GCC_VERSION}
+INTSTALL_PATH=$HOME/gcc/build-${GCC_VERSION}
 mkdir -p $INSTALL_PATH
 
-$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls --disable-libstdcxx-pch --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
-#$GCC_PATH/configure --prefix=${INTSTALL_PATH} --with-plugin-ld=/usr/local/bin/ld-plugin  --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
+$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls --disable-libstdcxx-pch --enable-lto --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib \
+        --disable-default-pie
 
 make -j$(getconf _NPROCESSORS_ONLN)
 
-make install 
+make install
 
 echo "export PATH=${INTSTALL_PATH}/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
