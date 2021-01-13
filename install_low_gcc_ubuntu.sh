a989d08ecede49d0ae0b9dfc739a51533770150a
rongxuanHong <389817374@qq.com>
2021-01-13 21:10:25 +0800

    Create install_low_gcc_ubuntu.sh

---
+++ install_low_gcc_ubuntu.sh
@@ -0,0 +1,50 @@
+#! /bin/bash -e
+#git clone https://github.com/gcc-mirror/gcc.git
+
+sudo apt-get install -y gcc make libncurses5-dev openssl libssl-dev build-essential pkg-config libc6-dev bison flex libelf-dev  texinfo
+HOME=/home/hrx
+SOURCE_BASE=https://mirrors.huaweicloud.com/gnu
+cd $HOME
+
+GCC_VERSION=gcc-4.9.4
+GMP=gmp-4.3.2
+MPC=mpc-0.8.1
+MPFR=mpfr-2.4.2
+if [ ! -d $GCC_VERSION ];then
+    wget $SOURCE_BASE/gcc/$GCC_VERSION/$GCC_VERSION.tar.gz && tar -zxvf $GCC_VERSION.tar.gz && rm -f $GCC_VERSION.tar.gz*
+fi
+
+GCC_PATH=$HOME/$GCC_VERSION
+cd $GCC_PATH
+
+
+if [ ! -d "mpfr" ];then
+        wget $SOURCE_BASE/mpfr/$MPFR.tar.gz
+        tar -zxvf $MPFR.tar.gz && mv $MPFR mpfr && rm -f $MPFR.tar.gz
+fi
+
+if [ ! -d "mpc" ];then
+        wget https://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz --no-check-certificate
+        tar -zxvf $MPC.tar.gz && mv $MPC mpc && rm -f $MPC.tar.gz
+fi
+
+if [ ! -d "gmp" ];then
+        wget $SOURCE_BASE/gmp/$GMP.tar.bz2
+        tar -jxvf $GMP.tar.bz2 && mv $GMP gmp && rm -f $GMP.tar.bz2
+fi
+
+mkdir -p gcc-build
+cd "gcc-build"
+
+INTSTALL_PATH=$HOME/gcc/build-${GCC_VERSION}
+mkdir -p $INTSTALL_PATH
+
+$GCC_PATH/configure --prefix=${INTSTALL_PATH} --enable-checking=yes --disable-nls --disable-libstdcxx-pch --enable-languages=c,c++ --disable-werror --disable-bootstrap --disable-multilib
+
+
+make -j$(getconf _NPROCESSORS_ONLN)
+
+make install
+
+#echo "export PATH=${INTSTALL_PATH}/bin:\$PATH" >> ~/.bashrc
+#source ~/.bashrc
